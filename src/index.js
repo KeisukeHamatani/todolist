981790a6efab55a00987be555bd1037245ae0a1f
Hamatani_Keisuke <hamatani@supersoftware.co.jp>
2021-01-12 20:26:58 +0900

    メッセージボックス追加

--- src/index.js
+++ src/index.js
@@ -3,27 +3,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-  class TodoList extends React.Component {
-    render() {
-      return (
-        <div className="todolist">
-          <div className="todolist">
-            {/*<aaaaa />*/}
-          </div>
-          <div className="todolist">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <TodoList />,
-    document.getElementById('root')
+/*function TextBox_dist(props) {
+  return (
+    <div>
+      <input type="text" />
+      <button>追加</button>
+    </div>
   );
-  
-  
+}*/
+
+const TextBox = (props) => {
+  return (
+    <div>
+      <input type="text" />
+      <button>追加</button>
+    </div>
+  );
+};
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [{           //task配列
+        task: null,       //入力したタクス格納
+        checked: false    //タスク完了判定
+      }],
+      index: 0,           //入力順インデックス
+      textmessage: null,  //テキストボックス入力文字
+    };
+  }
+
+  handleClic(){
+    const tasks = this.state.tasks.slice(0, this.state.index + 1); //現在のtask配列を保持
+    if(inputtext === null){
+      return;
+    }
+    this.setState({
+        /* テキストボックス内の文字(inputtext)を取得 */
+    });
+  }
+
+  render() {
+    return (
+      <div className="todolist">
+        <TextBox
+        /******/
+        onClick = {() => { this.handleClic()}}
+        />
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <TodoList />,
+  document.getElementById('root')
+);
+
