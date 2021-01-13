a8547e6c076c021aefee5e38e6d0e9a2e4c96e62
Hamatani_Keisuke <hamatani@supersoftware.co.jp>
2021-01-13 17:05:57 +0900

    メッセージ取得

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-/*function TextBox_dist(props) {
-  return (
-    <div>
-      <input type="text" />
-      <button>追加</button>
-    </div>
-  );
-}*/
-
 const TextBox = (props) => {
   return (
     <div>
-      <input type="text" />
-      <button>追加</button>
+      <div>
+        <input type="text" value={props.value} onChange={props.onChange} />
+        <button onClick={props.onClick}>
+          追加
+      </button>
+      </div>
     </div>
   );
 };
@@ -25,32 +20,58 @@ class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tasks: [{           //task配列
-        task: null,       //入力したタクス格納
-        checked: false    //タスク完了判定
-      }],
+      todolist: [],        //task配列
       index: 0,           //入力順インデックス
-      textmessage: null,  //テキストボックス入力文字
+      inputtext: "",      //テキストボックス入力文字
     };
   }
 
-  handleClic(){
-    const tasks = this.state.tasks.slice(0, this.state.index + 1); //現在のtask配列を保持
-    if(inputtext === null){
-      return;
+  handleClick() {
+    /* 現在のtask配列を保持 */
+    const tmp_todolist = this.state.todolist.slice(0, this.state.index+1);
+    const tmp_index = this.state.index;
+
+    /* nullまたはから文字でない場合 */
+    if (this.state.inputtext) {
+      /* todolistを更新 */
+      const new_task = {
+        task: this.state.inputtext,
+        checked: false,
+      };
+      this.setState({
+        todolist: tmp_todolist.concat(new_task),
+        inputtext: "",
+        index: tmp_index + 1,
+      });
     }
-    this.setState({
-        /* テキストボックス内の文字(inputtext)を取得 */
-    });
+
+    console.log(this.state.todolist);
+  }
+
+  handleChange(event) {
+    /* テキストボックス内の文字(inputtext)をstatus.inputetextへ取得 */
+    /* setStateで更新後、更新後の値で再レンダされる*/
+    this.setState({ inputtext: event.target.value });
   }
 
   render() {
+    const tmp_todolist = this.state.todolist;
+    const makelist = tmp_todolist.map((todo, num) => {
+      return(
+        <li key={num}> 
+          {todo.task} 
+        </li>
+      );}
+    );
+
     return (
       <div className="todolist">
         <TextBox
-        /******/
-        onClick = {() => { this.handleClic()}}
+          value={this.state.inputtext}
+          onChange={(event) => { this.handleChange(event) }}
+          onClick={() => { this.handleClick() }}
         />
+        <ol>{makelist}</ol>
       </div>
     );
   }
